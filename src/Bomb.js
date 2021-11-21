ff8b3d7e2697abaca7f156feaa778ba00950319b
Patrick Brennan <pbrennanmusic@gmail.com>
2021-11-20 22:45:15 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,20 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      secondsLeft: props.initialCount,
+    }
+  }
+
+  goBoom = () => {
+    if (this.state.secondsLeft === 0) {
+      return <h2>Boom!</h2>;
+    } else { return <h2>{this.state.secondsLeft} seconds left before I go boom!</h2> }
+  }
+
+  render() {
+    return <div>{this.goBoom()}</div>
+  }
+}
